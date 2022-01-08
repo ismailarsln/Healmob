67bd71ab13ef385d74e1354ad819a165e6027662
ismailarsln <ismail.arslan2019@bil.omu.edu.tr>
2022-01-08 18:31:28 +0300

    Add welcome screen

---
+++ lib/screens/welcome_sceen/welcomeScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class welcomeScreen extends StatelessWidget {
+  const welcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
